2838b07733aaf541e56cedc411a14f4ffedb60c8
Andy Hay <superandystar@gmail.com>
2016-11-14 22:48:04 -0500

    initial commit -- quickstart

---
+++ app/main.js
@@ -0,0 +1,9 @@
+// https://angular.io/docs/js/latest/tutorial/
+
+(function(app) {
+    document.addEventListener('DOMContentLoaded', function() {
+        ng.platformBrowserDynamic
+            .platformBrowserDynamic()
+            .bootstrapModule(app.AppModule);
+    });
+})(window.app || (window.app = {}));
